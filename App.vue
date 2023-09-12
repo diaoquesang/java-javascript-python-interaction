cc49888dcaa23249d3371786042d35ffaf955e33
Tristylral <114272662+Tristylral@users.noreply.github.com>
2023-09-12 23:27:20 +0800

    Add files via upload

--- App.vue
+++ App.vue
@@ -1,36 +1,59 @@
+<template>
+  <div class="big">
+    <el-upload
+      v-model:file-list="fileList"
+      class="upload-demo"
+      action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      list-type="picture"
+    >
+      <el-button type="primary">Click to upload</el-button>
+      <template #tip>
+        <div class="el-upload__tip">少于500kb的jpg/png格式图片</div>
+      </template>
+    </el-upload>
+  </div>
+</template>
+
 <script lang="ts" setup>
 import { ref } from "vue";
-import type { UploadInstance } from "element-plus";
 
-const uploadRef = ref<UploadInstance>();
+import type { UploadProps, UploadUserFile } from "element-plus";
 
-const submitUpload = () => {
-  uploadRef.value!.submit();
+const fileList = ref<UploadUserFile[]>([
+  {
+    //示例图片
+    name: "请稍候",
+    url: "/加载动画.gif",
+  },
+]);
+
+const handleRemove: UploadProps["onRemove"] = (uploadFile, uploadFiles) => {
+  console.log(uploadFile, uploadFiles);
+};
+
+const handlePreview: UploadProps["onPreview"] = (file) => {
+  console.log(file);
 };
 </script>
 
-<template>
-  <el-upload
-    ref="uploadRef"
-    class="upload-demo"
-    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-    :auto-upload="false"
-  >
-    <template #trigger>
-      <el-button type="primary">select file</el-button>
-    </template>
-
-    <el-button class="ml-3" type="success" @click="submitUpload">
-      upload to server
-    </el-button>
-
-    <template #tip>
-      <div class="el-upload__tip">
-        jpg/png files with a size less than 500kb
-      </div>
-    </template>
-  </el-upload>
-</template>
-
 <style scoped>
+:deep(.el-button) {
+  width: 200px;
+  height: 50px;
+  margin-left: 100px;
+}
+
+.el-upload__tip {
+  font-size: 16px;
+  margin-left: 100px;
+}
+
+.big {
+  width: 400px;
+  height: 300px;
+  border: 1px solid black;
+  border-radius: 5px;
+}
 </style>
